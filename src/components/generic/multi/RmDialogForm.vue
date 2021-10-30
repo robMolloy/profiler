31e7ae122ab004a69691c759a49ad5171a881b91
robMolloy <robmolloy@hotmail.co.uk>
2021-10-30 03:32:23 +0100

    More readable RmDialogForm

--- src/components/generic/multi/RmDialogForm.vue
+++ src/components/generic/multi/RmDialogForm.vue
@@ -1,25 +1,18 @@
 <template>
-  <RmDialogBase>
-    <RmCardForm
-      :title="title"
-      @submit="(...props)=>$emit('submit', ...props)"
-      @reset="(...props)=>$emit('reset', ...props)"
+  <RmDialogCardHeader>
+    <RmForm
+      @submit="(...props)=>$emit('submit',...props)"
+      @reset="(...props)=>$emit('reset',...props)"
     >
       <slot />
-      <template #headerRight>
-        <RmIconClose v-close-popup />
-      </template>
-    </RmCardForm>
-  </RmDialogBase>
+    </RmForm>
+  </RmDialogCardHeader>
 </template>
 
 <script setup>
-import { useAttrs, defineEmits } from 'vue';
+import { defineEmits } from 'vue';
+import RmDialogCardHeader from '../single/dialogs/RmDialogCardHeader.vue';
+import RmForm from './RmForm.vue';
 
-import RmIconClose from '../single/icons/RmIconClose.vue';
-import RmCardForm from './RmCardForm.vue';
-import RmDialogBase from '../single/dialogs/RmDialogBase.vue';
-
-const { title } = useAttrs();
 defineEmits(['submit', 'reset']);
 </script>
