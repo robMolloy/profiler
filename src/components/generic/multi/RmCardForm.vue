c21384750050d42be33c6da3c414d0fde1fb45b8
robMolloy <robmolloy@hotmail.co.uk>
2021-11-26 20:46:11 +0000

    refactor RmCardForm.vue

--- src/components/generic/multi/RmCardForm.vue
+++ src/components/generic/multi/RmCardForm.vue
@@ -1,27 +1,38 @@
 <template>
-  <RmCardHeader>
-    <template #headerLeft>
-      <slot name="headerLeft" />
-    </template>
-    <template #headerRight>
-      <slot name="headerRight" />
-    </template>
+  <RmCardBase>
+    <RmHeaderBar
+      v-if="props.showHeader"
+      :title="props.title"
+    >
+      <template #left>
+        <slot name="headerLeft" />
+      </template>
+      <template #right>
+        <slot name="headerRight" />
+      </template>
+    </RmHeaderBar>
 
     <RmForm
-      @submit="(...props)=>$emit('submit',...props)"
-      @reset="(...props)=>$emit('reset',...props)"
+      @submit="(...submitProps)=>$emit('submit',...submitProps)"
+      @reset="(...resetProps)=>$emit('reset',...resetProps)"
     >
       <slot />
     </RmForm>
-  </RmCardHeader>
+  </RmCardBase>
 </template>
 
 <script setup>
-import { defineEmits } from 'vue';
+import { defineEmits, defineProps } from 'vue';
 
-import RmCardHeader from '../single/cards/RmCardHeader.vue';
+// import RmCardHeader from '../single/cards/RmCardHeader.vue';
 import RmForm from './RmForm.vue';
+import RmCardBase from '../single/cards/RmCardBase.vue';
+import RmHeaderBar from '../single/bars/RmHeaderBar.vue';
 
 defineEmits(['submit', 'reset']);
+const props = defineProps({
+  showHeader: { default: () => true, type: Boolean },
+  title: { default: '', type: String },
+});
 
 </script>
